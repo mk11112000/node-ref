85b25ce5b155adc1bcc7ed67f698d1bd99db1a59
mk11112000 <65941178+mk11112000@users.noreply.github.com>
2020-09-15 01:43:45 +0530

    Add files via upload

---
+++ nodemysql/app.js
@@ -0,0 +1,35 @@
+const mysql = require("mysql");
+
+const express = require("express");
+
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "KM11321/06",
+  database: "nodemysql",
+  port: 3306,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Connected!");
+});
+
+const app = express();
+
+const port = 3000;
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/createdb", (req, res) => {
+  let sql = "CREATE DATABASE nodemysql";
+
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    else res.send(result);
+    console.log(result);
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
